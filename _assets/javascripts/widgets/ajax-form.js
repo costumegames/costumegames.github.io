27402c3079b7a45d506ce1d0deb57349d3909ee4
Adam Bouqdib <adam@abemedia.co.uk>
2015-07-16 18:01:01 +0000

    squash! Re-order items

--- _assets/javascripts/widgets/ajax-form.js
+++ _assets/javascripts/widgets/ajax-form.js
@@ -1,35 +1,31 @@
-$( ".donforms" ).on('submit', function( event ) {
+$( ".donforms" ).submit(function( event ) {
     event.preventDefault();
+    
     var $form = $(this),
         data = $form.serialize(),
-        data2 = $form.serializeArray(),
+        action = $form.attr("action"),
         $input = $form.find('input, select, textarea').prop('disabled', 1),
         $btn = $form.find('button').button('loading');
-        
-    $.ajax({
-        url: $form.attr("action"),
-        type: 'post',
-        data: data,
-        accepts: {
-            json: 'application/json'
-        },
-    }).always(function(data) {
-        $input.prop('disabled', 0);
-        $btn.button('reset');
-        if (typeof Recaptcha != "undefined") {
-            Recaptcha.reload();
-        }
-        $('html,body').animate({ scrollTop: $form.offset().top }, 1000);
-    })
-    .done(function(data) {
-        if(data.status == 'error') {
-            toastr.error(data.message);
-        } else {
-            $form[0].reset();
-            toastr.success('Message sent successfully. We will be in touch shortly.');
-        }
-    })
-    .fail(function(data) {
-        toastr.error(data.message);
-    });
+    
+    var send = $.post(action, data)
+        .always(function(data) {
+            $input.prop('disabled', 0);
+            $btn.button('reset');
+            $form.find('.alert').remove();
+            if (typeof Recaptcha != "undefined") {
+                Recaptcha.reload();
+            }
+            $('html,body').animate({ scrollTop: $('#content').offset().top }, 1000);
+        })
+        .done(function(data) {
+            if(data.status == 'error') {
+                $form.prepend('<div class="alert alert-danger">Error: ' + data.message + '</div>');
+            } else {
+                $form[0].reset();
+                $form.prepend('<div class="alert alert-success">Message sent successfully. We will be in touch shortly.</div>');
+            }
+        })
+        .fail(function(data) {
+            $form.prepend('<div class="alert alert-danger">Error: ' + data.message + '</div>');
+        });
 });
