a342ee9072a0e75cae5564e34f95a9a6e5e8f9c4
Monty Anderson <monty.linux@gmail.com>
2015-08-17 19:20:01 +0100

    schedule updates

--- _assets/javascripts/widgets/common.js
+++ _assets/javascripts/widgets/common.js
@@ -24,23 +24,32 @@ $('a[href*=#]:not([href=#],.carousel-control,[data-toggle])').click( function() 
     }
 });
 
+if(location.pathname == "/events.html") {
+    $(".img-3x4").each(function() {
+        switch(Math.floor(Math.random()*4)) {
+            case 0:
+                $(this).addClass("bg-success");
+                break;
 
-$(".img-3x4").each(function() {
-    switch(Math.floor(Math.random()*4)) {
-        case 0:
-            $(this).addClass("bg-success");
-            break;
+            case 1:
+                $(this).addClass("bg-warning");
+                break;
 
-        case 1:
-            $(this).addClass("bg-warning");
-            break;
+            case 2:
+                $(this).addClass("bg-danger");
+                break;
 
-        case 2:
-            $(this).addClass("bg-danger");
-            break;
+            case 3:
+                $(this).addClass("bg-primary");
+                break;
+        }
+    });
 
-        case 3:
-            $(this).addClass("bg-primary");
-            break;
-    }
-});
+    $(".col-sm-3").find("h3").each(function() {
+        $(this).parent().css("cursor", "pointer");
+
+        $(this).parent().click(function() {
+            location.replace("/schedule.html#" + $(this).find("h3").first().text().toLowerCase());
+        });
+    });
+}
